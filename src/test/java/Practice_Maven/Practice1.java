ca1f12eec8f5ff207d973e1c0d12ffb99f084f13
Shyam <Shyam@DESKTOP-OUI7JLC>
2023-01-19 13:05:30 +0530

    Chota Bhai Jaan

---
+++ src/test/java/Practice_Maven/Practice1.java
@@ -0,0 +1,11 @@
+package Practice_Maven;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void bhaiJaan() {
+		System.out.println("Chotta bhai jaan aaya");
+	}
+
+}
